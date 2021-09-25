eafeb3ec92b33c0d821734903e130b1c1a403055
nailatul.fadhilah <nailatul.fadhilah@hukumonline.com>
2021-09-25 16:46:53 +0700

    add examples and edit the copywriting

--- gradio_face_recognition.py
+++ gradio_face_recognition.py
@@ -2,27 +2,17 @@ import gradio as gr
 from face_recog import detect_face
 
 title = "Face Recognition"
-desc = "--"
-# examples = [
-#     ["example_images/3.png"],
-#     ["example_images/2.png"],
-#     ["example_images/1.png"],
-# ]
+desc = "Upload a picture of each person you already know and also upload the picture that you want to identify"
 
-examples = [
-    ["images/taylor.jpg"],
-    ["images/selena.jpg"],
-    ["Taylor Swift"],
-    ["Selena Gomez"],
-    ["images/taylorselena.jpg"]
-]
+examples = [["images/taylor.jpg", "Taylor Swift", "images/selena.jpg", "Selena Gomez", "images/taylorselena.jpg"]]
+
 inputs = [gr.inputs.Image(label="Picture of Person 1", type="file"),
           gr.inputs.Textbox(label="Name of Person 1"),
           gr.inputs.Image(label="Picture of Person 2", type="file"),
           gr.inputs.Textbox(label="Name of Person 2"),
           gr.inputs.Image(label="Picture of Person 1 and 2", type="file")
         ]
-outputs = gr.outputs.Image(label="Predicted Face", type="pil")
+outputs = gr.outputs.Image(label="Result", type="pil")
 gr.Interface(fn=detect_face, inputs=inputs, outputs=outputs, title=title, description=desc, 
-             #examples=examples,
+             examples=examples,
              allow_flagging=False, server_name="0.0.0.0", server_port=8000).launch()
