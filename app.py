3f9adf5675eb7a6d1b6c6d00c307b6f7d67dbda7
nailatul.fadhilah <nailatul.fadhilah@hukumonline.com>
2021-09-26 14:30:09 +0700

    remove flag and update desc

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 from face_recog import detect_face
 
 title = "Face Recognition"
-desc = "Upload a picture of 2 people you already know and also upload the picture that you want to identify"
+desc = "Upload the pictures of 2 people you already know and also upload the picture that you want to identify"
 
 examples = [["images/taylor.jpg", "Taylor Swift", "images/selena.jpg", "Selena Gomez", "images/taylorselena.jpg"]]
 
@@ -14,4 +14,4 @@ inputs = [gr.inputs.Image(label="Picture of Person 1", type="file"),
          ]
 outputs = gr.outputs.Image(label="Result", type="pil")
 gr.Interface(fn=detect_face, inputs=inputs, outputs=outputs, title=title, description=desc,
-             examples=examples, capture_session=True).launch()
+             examples=examples).launch()
